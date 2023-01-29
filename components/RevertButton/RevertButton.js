5371e967c1bbc5acf1d0a8c4d5cfa9841fb6b88d
ThomasFoydel <thomasjfoydel@gmail.com>
2023-01-29 17:38:26 -0500

    migrated revert button styles

--- components/RevertButton/RevertButton.js
+++ components/RevertButton/RevertButton.js
@@ -1,43 +1,43 @@
-import React, { useContext } from 'react';
-import './RevertButton.scss';
-import { CTX } from 'context/Store';
-import Axios from 'axios';
+import Axios from 'axios'
+import React, { useContext } from 'react'
+import styles from './RevertButton.module.scss'
+import { CTX } from '../../context/Store'
 
 const RevertButton = ({ openAuth }) => {
-  const [appState, updateState] = useContext(CTX);
+  const [appState, updateState] = useContext(CTX)
 
   const revert = () => {
     if (!appState.isLoggedIn) {
-      return openAuth();
+      return openAuth()
     }
-    const foundToken = localStorage.getItem('cleanbreak-token');
-    let { currentPreset } = appState;
+    const foundToken = localStorage.getItem('cleanbreak-token')
+    const { currentPreset } = appState
 
     Axios.get(`/presets/revert/${currentPreset}`, {
-      headers: { 'x-auth-token': foundToken },
+      headers: { 'x-auth-token': foundToken }
     })
       .then((result) => {
         if (result.data.err) {
-          console.log('err: ', result.data.err);
+          console.error('err: ', result.data.err)
         } else {
-          let { presetFromDb } = result.data;
-          let { name, params } = presetFromDb;
+          const { presetFromDb } = result.data
+          const { name, params } = presetFromDb
           updateState({
             type: 'LOAD_PRESET',
             payload: { value: params },
-            current: name,
-          });
+            current: name
+          })
         }
       })
       .catch((err) => {
-        console.log('revert error: ', err);
-      });
-  };
+        console.log('revert error: ', err)
+      })
+  }
   return (
-    <button className='revert-btn' onClick={revert}>
+    <button className={styles.revertBtn} onClick={revert}>
       revert
     </button>
-  );
-};
+  )
+}
 
-export default RevertButton;
+export default RevertButton
