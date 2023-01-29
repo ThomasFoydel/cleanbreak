140b41dee5e5a00ecd71e0d4baf89f3467222d02
ThomasFoydel <thomasjfoydel@gmail.com>
2023-01-29 17:46:54 -0500

    migrated square styles

--- components/Square/Square.js
+++ components/Square/Square.js
@@ -1,40 +1,42 @@
-import React from 'react';
-import { useSpring, config, animated } from 'react-spring';
-import './Square.scss';
+import React from 'react'
+import { useSpring, config, animated } from 'react-spring'
+import styles from './Square.module.scss'
 
 const Square = ({ handleClick, instrument, step, value, handleMouseEnter }) => {
-  let color;
-  let size;
+  let color
+  let size
   if (value === 0) {
-    color = 'rgb(182, 182, 182)';
-    size = 0.6;
+    color = 'rgb(182, 182, 182)'
+    size = 0.6
   } else if (value === 1) {
-    color = 'rgb(23, 23, 23)';
-    size = 0.85;
+    color = 'rgb(23, 23, 23)'
+    size = 0.85
   } else if (value === 2) {
-    color = 'rgb(150, 8, 8)';
-    size = 1;
+    color = 'rgb(150, 8, 8)'
+    size = 1
   }
+
   const animationProps = useSpring({
     backgroundColor: color,
     transform: `scale(${size})`,
-    config: config.wobbly,
-  });
+    config: config.wobbly
+  })
 
-  const handleSquareClick = (e) => {
-    handleClick({ instrument, step, value });
-  };
-  const handleSquareMouseEnter = (e) => {
-    handleMouseEnter({ instrument, step, value });
-  };
+  const handleSquareClick = () => {
+    handleClick({ instrument, step, value })
+  }
+
+  const handleSquareMouseEnter = () => {
+    handleMouseEnter({ instrument, step, value })
+  }
+
   return (
     <animated.div
       style={animationProps}
       onMouseDown={handleSquareClick}
-      className='square'
-      onMouseEnter={handleSquareMouseEnter}
-    ></animated.div>
-  );
-};
+      className={styles.square}
+      onMouseEnter={handleSquareMouseEnter}></animated.div>
+  )
+}
 
-export default Square;
+export default Square
