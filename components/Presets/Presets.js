0997353215b55b8095bb809569ea3269ad76e759
ThomasFoydel <thomasjfoydel@gmail.com>
2023-01-29 17:23:47 -0500

    migrated presets styles

--- components/Presets/Presets.js
+++ components/Presets/Presets.js
@@ -1,7 +1,9 @@
-import React, { useContext, useState, useEffect } from 'react';
-import Axios from 'axios';
-import { CTX } from 'context/Store';
-import './Presets.scss';
+import Axios from 'axios'
+import React, { useContext, useState, useEffect } from 'react'
+import styles from './Presets.module.scss'
+import { CTX } from '../../context/Store'
+import cn from 'classnames'
+
 const filterOut = [
   'presets',
   'clickActive',
@@ -10,39 +12,39 @@ const filterOut = [
   'playing',
   'user',
   'currentPreset',
-  'keyboardOctaveOffset',
-];
+  'keyboardOctaveOffset'
+]
 
 const filter = (state) => {
   return Object.keys(state)
     .filter((key) => !filterOut.includes(key))
     .reduce((obj, key) => {
-      obj[key] = state[key];
-      return obj;
-    }, {});
-};
+      obj[key] = state[key]
+      return obj
+    }, {})
+}
 
 const Presets = ({ openAuth }) => {
-  const [appState, updateState] = useContext(CTX);
-  const [display, setDisplay] = useState({});
-  const [errorMessage, setErrorMessage] = useState('');
-  const [presetName, setPresetName] = useState('');
-  const foundToken = localStorage.getItem('cleanbreak-token');
-  let { isLoggedIn } = appState;
+  const [appState, updateState] = useContext(CTX)
+  const [display, setDisplay] = useState({})
+  const [errorMessage, setErrorMessage] = useState('')
+  const [presetName, setPresetName] = useState('')
+  const foundToken = localStorage.getItem('cleanbreak-token')
+  let { isLoggedIn } = appState
 
   const open = (id) => {
-    setDisplay({ [id]: true });
-  };
+    setDisplay({ [id]: true })
+  }
   const closeAll = () => {
-    setDisplay({});
-  };
+    setDisplay({})
+  }
 
   const saveNew = async () => {
     if (!presetName) {
-      closeAll();
-      return setErrorMessage('name value required');
+      closeAll()
+      return setErrorMessage('name value required')
     }
-    const filteredState = filter(appState);
+    const filteredState = filter(appState)
     Axios.post(
       '/presets/newsave',
       { name: presetName, state: filteredState, username: appState.user.name },
@@ -50,53 +52,55 @@ const Presets = ({ openAuth }) => {
     )
       .then((result) => {
         if (result.data.err) {
-          closeAll();
-          setPresetName('');
-          setErrorMessage(result.data.err);
+          closeAll()
+          setPresetName('')
+          setErrorMessage(result.data.err)
         } else {
           updateState({
             type: 'UPDATE_PRESETS',
             payload: {
               presets: result.data.presets,
-              current: result.data.current,
-            },
-          });
-          closeAll();
-          setErrorMessage('new preset saved!');
-          setPresetName('');
+              current: result.data.current
+            }
+          })
+          closeAll()
+          setErrorMessage('new preset saved!')
+          setPresetName('')
         }
       })
-      .catch((err) => console.log('save preset error: ', err));
-  };
+      .catch((err) => console.log('save preset error: ', err))
+  }
+
   const saveOver = async () => {
-    const filteredState = filter(appState);
+    const filteredState = filter(appState)
     Axios.post(
       '/presets/save',
       {
         name: appState.currentPreset,
         state: filteredState,
-        username: appState.user.name,
+        username: appState.user.name
       },
       { headers: { 'x-auth-token': foundToken } }
     )
       .then((result) => {
         if (result.data.err) {
-          closeAll();
-          setErrorMessage(result.data.err);
+          closeAll()
+          setErrorMessage(result.data.err)
         } else {
           updateState({
             type: 'UPDATE_PRESETS',
             payload: {
               presets: result.data.presets,
-              current: result.data.current,
-            },
-          });
-          closeAll();
-          setErrorMessage('preset saved!');
+              current: result.data.current
+            }
+          })
+          closeAll()
+          setErrorMessage('preset saved!')
         }
       })
-      .catch((err) => console.log('save preset error: ', err));
-  };
+      .catch((err) => console.log('save preset error: ', err))
+  }
+
   const deletePreset = (data) => {
     Axios.post(
       '/presets/delete',
@@ -105,42 +109,42 @@ const Presets = ({ openAuth }) => {
     )
       .then((result) => {
         if (result.data.err) {
-          closeAll();
-          setErrorMessage(result.data.err);
+          closeAll()
+          setErrorMessage(result.data.err)
         } else {
           updateState({
             type: 'UPDATE_PRESETS',
             payload: {
               presets: result.data.presets,
-              current: result.data.current,
-            },
-          });
+              current: result.data.current
+            }
+          })
           updateState({
             type: 'LOAD_PRESET',
             text: result.data.current,
-            payload: result.data.presets[result.data.newCurrentIndex],
-          });
-          closeAll();
-          setErrorMessage('preset deleted!');
+            payload: result.data.presets[result.data.newCurrentIndex]
+          })
+          closeAll()
+          setErrorMessage('preset deleted!')
         }
       })
       .catch((err) => {
-        console.log('save preset error: ', err);
-      });
-  };
+        console.log('save preset error: ', err)
+      })
+  }
 
   useEffect(() => {
     setTimeout(() => {
-      setErrorMessage('');
-    }, 1900);
-  }, [errorMessage]);
+      setErrorMessage('')
+    }, 1900)
+  }, [errorMessage])
 
   const handleTextInput = (e) => {
-    setPresetName(e.target.value);
-  };
+    setPresetName(e.target.value)
+  }
 
   return (
-    <div className='presets'>
+    <div className={styles.presets}>
       <button onClick={() => (isLoggedIn ? open('saveNew') : openAuth())}>
         save as
       </button>
@@ -152,46 +156,46 @@ const Presets = ({ openAuth }) => {
       </button>
 
       {display.saveNew && (
-        <div className='preset-op-open save-new'>
+        <div className={cn(styles.presetOpOpen, styles.saveNew)}>
           <input
             type='text'
             onChange={handleTextInput}
             placeholder='preset name'
           />
-          <button className='confirm' onClick={saveNew}>
+          <button className={styles.confirm} onClick={saveNew}>
             save
           </button>
-          <button className='cancel' onClick={closeAll}>
+          <button className={styles.cancel} onClick={closeAll}>
             cancel
           </button>
         </div>
       )}
 
       {display.saveOver && (
-        <div className='preset-op-open save-over'>
-          <button className='confirm' onClick={saveOver}>
+        <div className={cn(styles.presetOpOpen, styles.saveOver)}>
+          <button className={styles.confirm} onClick={saveOver}>
             confirm save
           </button>
-          <button className='cancel' onClick={closeAll}>
+          <button className={styles.cancel} onClick={closeAll}>
             cancel
           </button>
         </div>
       )}
 
       {display.delete && (
-        <div className='preset-op-open delete'>
-          <button className='confirm' onClick={deletePreset}>
+        <div className={cn(styles.presetOpOpen, styles.delete)}>
+          <button className={styles.confirm} onClick={deletePreset}>
             confirm delete
           </button>
-          <button className='cancel' onClick={closeAll}>
+          <button className={styles.cancel} onClick={closeAll}>
             cancel
           </button>
         </div>
       )}
 
-      <div className='err-msg'>{errorMessage}</div>
+      <div className={styles.errMsg}>{errorMessage}</div>
     </div>
-  );
-};
+  )
+}
 
-export default Presets;
+export default Presets
