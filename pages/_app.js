3fb5ec364b56c0b7060e4d13ecace89b63069a8f
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-01 04:33:31 -0500

    meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,45 @@ const Store = dynamic(() => import('../context/Store'), { ssr: false })
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta
+          property='og:image:url'
+          content='https://cleanbreak.vercel.app/preview.jpg'
+        />
+
+        <meta property='og:image:width' content='1800px' />
+        <meta property='og:image:height' content='1800px' />
+        <meta property='og:title' content='CLEAN BREAK' />
+        <meta
+          property='og:description'
+          content='A drum machine for the browser.'
+        />
+        <meta property='og:url' content='https://cleanbreak.vercel.app' />
+        <meta property='og:type' content='website' />
+
+        <meta charSet='utf-8' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#000000' />
+
+        <link rel='apple-touch-icon' href='/logo192.png' />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://cleanbreak.vercel.app' />
+        <meta property='twitter:title' content='CLEAN BREAK' />
+        <meta
+          property='twitter:description'
+          content='A drum machine for the browser.'
+        />
+        <meta
+          property='twitter:image'
+          content='https://cleanbreak.vercel.app/preview.jpg'
+        />
+
+        <title>CLEAN BREAK</title>
+        <meta name='title' content='CLEAN BREAK' />
+        <meta name='description' content='A drum machine for the browser.' />
+      </Head>
       <Store>
         <Component {...pageProps} />
         <div id='modal' />
