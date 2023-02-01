e472b9e87e45280c08ae76db21274e6ffb29c75a
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-01 13:32:45 -0500

    removed autoclose false from toast container

--- pages/_app.js
+++ pages/_app.js
@@ -52,7 +52,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       <Store>
         <Component {...pageProps} />
         <div id='modal' />
-        <ToastContainer position='bottom-right' autoClose={false} />
+        <ToastContainer position='bottom-right' />
       </Store>
     </SessionProvider>
   )
